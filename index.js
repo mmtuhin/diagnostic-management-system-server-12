0cf560767b318d8b35d0cd0bf5df3e2dd81ae3be
Tuhin Hossain <mmtuhinhossain@gmail.com>
2023-12-04 01:15:09 +0600

    Payment can be done using stripe

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 // MiddleWare
@@ -256,7 +257,22 @@ async function run() {
 
 
     // Payments
-    
+    // payment intent
+    app.post('/create-payment-intent',verifyToken, async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      console.log(amount, 'amount inside the intent')
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    });
 
 
     // Send a ping to confirm a successful connection
