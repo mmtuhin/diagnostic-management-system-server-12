0775c3eae3a725a654e9ab3dc43060e3aa2b67a4
Tuhin Hossain <mmtuhinhossain@gmail.com>
2023-12-04 16:13:17 +0600

    Before deploy

--- index.js
+++ index.js
@@ -65,12 +65,12 @@ async function run() {
       const email = req.decoded.email;
       const query = { email: email };
       const user = await userCollection.findOne(query);
-      const isAdmin = user?.role === 'admin';
+      const isAdmin = user?.role === "admin";
       if (!isAdmin) {
-        return res.status(403).send({ message: 'forbidden access' });
+        return res.status(403).send({ message: "forbidden access" });
       }
       next();
-    }
+    };
 
     //find all district
     app.get("/district", async (req, res) => {
@@ -91,7 +91,6 @@ async function run() {
       res.send(result);
     });
 
-
     //get all users
     app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
       // console.log(req.decoded.email);
@@ -100,17 +99,22 @@ async function run() {
     });
 
     //Make a user an ADMIN
-    app.patch("/users/admin/:id", verifyToken, verifyAdmin, async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: new ObjectId(id) };
-      const updatedDoc = {
-        $set: {
-          role: "admin",
-        },
-      };
-      const result = await userCollection.updateOne(filter, updatedDoc);
-      res.send(result);
-    });
+    app.patch(
+      "/users/admin/:id",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updatedDoc = {
+          $set: {
+            role: "admin",
+          },
+        };
+        const result = await userCollection.updateOne(filter, updatedDoc);
+        res.send(result);
+      }
+    );
 
     //Checking if the logging user is ADMIN or NOT
     app.get("/users/admin/:email", verifyToken, async (req, res) => {
@@ -119,30 +123,35 @@ async function run() {
       console.log(req.decoded);
 
       if (email !== req.decoded.email) {
-        return res.status(403).send({ message: 'forbidden access' })
+        return res.status(403).send({ message: "forbidden access" });
       }
 
       const query = { email: email };
       const user = await userCollection.findOne(query);
       let admin = false;
       if (user) {
-        admin = user?.role === 'admin';
+        admin = user?.role === "admin";
       }
       res.send({ admin });
-    })
+    });
 
     //Block a user
-    app.patch("/users/block/:id", verifyToken, verifyAdmin, async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: new ObjectId(id) };
-      const updatedDoc = {
-        $set: {
-          status: "blocked",
-        },
-      };
-      const result = await userCollection.updateOne(filter, updatedDoc);
-      res.send(result);
-    });
+    app.patch(
+      "/users/block/:id",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updatedDoc = {
+          $set: {
+            status: "blocked",
+          },
+        };
+        const result = await userCollection.updateOne(filter, updatedDoc);
+        res.send(result);
+      }
+    );
 
     //Delete a user form DB
     app.delete("/users/:id", verifyToken, verifyAdmin, async (req, res) => {
@@ -153,8 +162,8 @@ async function run() {
     });
 
     // save a test to database
-    app.post("/tests",  async (req, res) => {
-      const test = req.body
+    app.post("/tests", async (req, res) => {
+      const test = req.body;
       // console.log(test);
       const result = await testCollection.insertOne(test);
       res.send(result);
@@ -168,25 +177,27 @@ async function run() {
     });
 
     // get a specific test Data
-    app.get('/testdetails/:id', async (req, res) => {
+    app.get("/testdetails/:id", async (req, res) => {
       const testId = req.params.id;
       const query = { _id: new ObjectId(testId) };
       try {
-        const result = await testCollection.findOne(query); 
-        res.send(result)
+        const result = await testCollection.findOne(query);
+        res.send(result);
       } catch (error) {
-        console.error('Error fetching test details:', error);
-        res.status(500).json({ message: 'Failed to fetch test details' });
+        console.error("Error fetching test details:", error);
+        res.status(500).json({ message: "Failed to fetch test details" });
       }
     });
 
     //get filtered dates from present to future
-    app.get('/availabletests', async(req, res) => {
-      const today = new Date()
-      const result = await testCollection.find({testStartDate: {$gte:today.toISOString()} }).toArray()
-      console.log(result)
-      res.send(result)
-    })
+    app.get("/availabletests", async (req, res) => {
+      const today = new Date();
+      const result = await testCollection
+        .find({ testStartDate: { $gte: today.toISOString() } })
+        .toArray();
+      console.log(result);
+      res.send(result);
+    });
 
     // Delete a test
     app.delete("/tests/:id", verifyToken, verifyAdmin, async (req, res) => {
@@ -197,28 +208,30 @@ async function run() {
     });
 
     // Add A Banner
-    app.post("/banners",verifyToken,  async (req, res) => {
-      const banner = {...req.body, isActive: false}
+    app.post("/banners", verifyToken, async (req, res) => {
+      const banner = { ...req.body, isActive: false };
       console.log(banner);
       const result = await bannerCollection.insertOne(banner);
       res.send(result);
     });
 
     // Get All the banners
-    app.get("/banners",verifyToken, verifyAdmin,  async (req, res) => {
+    app.get("/banners", verifyToken, verifyAdmin, async (req, res) => {
       // console.log(req.decoded.email);
       const result = await bannerCollection.find().toArray();
       res.send(result);
     });
 
     //Find Active Banner
-    app.get('/banners/active', async (req, res) => {
+    app.get("/banners/active", async (req, res) => {
       try {
-        const activeBanner = await bannerCollection.find({ isActive: true }).toArray();
-        res.send(activeBanner)
+        const activeBanner = await bannerCollection
+          .find({ isActive: true })
+          .toArray();
+        res.send(activeBanner);
       } catch (error) {
-        console.error('Error fetching active banners:', error);
-        res.status(500).json({ message: 'Failed to fetch active banner' });
+        console.error("Error fetching active banners:", error);
+        res.status(500).json({ message: "Failed to fetch active banner" });
       }
     });
 
@@ -232,15 +245,14 @@ async function run() {
 
     // make a Banner Active and others deactive status false
     app.patch("/banners/activate/:id", async (req, res) => {
-      const id = req.params.id;   
+      const id = req.params.id;
       try {
-    
         // Find the banner with the given ID and update its isActive to true
         const result = await bannerCollection.updateOne(
           { _id: new ObjectId(id) },
           { $set: { isActive: true } }
         );
-    
+
         // Set isActive to false for other banners except the one updated
         if (result.modifiedCount > 0) {
           await bannerCollection.updateMany(
@@ -248,54 +260,99 @@ async function run() {
             { $set: { isActive: false } }
           );
         }
-    
+
         res.status(200).json({ message: "Banner updated successfully" });
       } catch (error) {
         console.error("Error updating banner:", error);
-        res.status(500).json({ message: "An error occurred while updating banner" });
+        res
+          .status(500)
+          .json({ message: "An error occurred while updating banner" });
       }
     });
 
-
     // Payments
     // payment intent
-    app.post('/create-payment-intent',verifyToken, async (req, res) => {
+    app.post("/create-payment-intent", verifyToken, async (req, res) => {
       const { price } = req.body;
       const amount = parseInt(price * 100);
-      console.log(amount, 'amount inside the intent')
+      console.log(amount, "amount inside the intent");
 
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
-        currency: 'usd',
-        payment_method_types: ['card']
+        currency: "usd",
+        payment_method_types: ["card"],
       });
 
       res.send({
-        clientSecret: paymentIntent.client_secret
-      })
+        clientSecret: paymentIntent.client_secret,
+      });
     });
 
-
     //bookings
-    app.post("/bookings",verifyToken,  async (req, res) => {
+    app.post("/bookings", verifyToken, async (req, res) => {
       const id = req.body.testId;
       const filter = { _id: new ObjectId(id) };
       const test = await testCollection.findOne(filter);
       if (test && test.slots > 0) {
         const updatedDoc = {
-          $set: { slots: test.slots - 1 }
+          $set: { slots: test.slots - 1 },
         };
-       const res2 = await testCollection.updateOne(filter, updatedDoc);
-       console.log(res2);
+        const res2 = await testCollection.updateOne(filter, updatedDoc);
+        console.log(res2);
 
-      const booking = {...req.body, reportStatus: 'pending'}
-      console.log(booking);
-      const result1 = await bookingCollection.insertOne(booking);
-      res.send(result1)
+        const booking = { ...req.body, reportStatus: "pending" };
+        console.log(booking);
+        const result1 = await bookingCollection.insertOne(booking);
+        res.send(result1);
       }
-    })
+    });
+
+    //Reservations of a single test
+    app.get("/reservations/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { testId: id };
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // Delete a Reservation
+    app.delete("/reservations/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    //Update reservation result
+    app.patch("/reservations/:id", async (req, res) => {
+      const id = req.params.id;
+      // const { pdfLink } = req.body.pdfLink;
+      console.log(id);
+      console.log(req.body.pdfLink);
+      try {
+        // Find the banner with the given ID and update its isActive to true
+        const result = await bookingCollection.updateOne(
+          { _id: new ObjectId(id) },
+          { $set: { pdfLink: req.body.pdfLink, reportStatus: "done" } }
+        );
 
 
+        if (result.modifiedCount > 0) {
+          res.send(result)
+        } else {
+          res
+            .status(404)
+            .json({ message: "No booking found with the provided ID" });
+        }
+      } catch (error) {
+        console.error("Error updating booking:", error);
+        res
+          .status(500)
+          .json({ message: "An error occurred while updating booking" });
+      }
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
